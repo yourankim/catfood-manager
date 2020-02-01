46baac83fcf62a9c6dcd068ba6dc44fbd83b5943
yourankim <yourankim.tech@gmail.com>
2020-02-01 17:48:53 +0900

    Profile컴포넌트의 img에 alt 속성 추가

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,7 @@ function Profile() {
 
   return (
     <div className="profile">
-      <img className="profile-image" src={profile}/>
+      <img className="profile-image" src={profile} alt="곳간 주인 고양이의 사진"/>
       <div>곳간 주인: 노랑이</div>
     </div>
   );
