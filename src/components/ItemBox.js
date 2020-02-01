3df6fb7c5df98834477bc9c62f98dec647f952e9
yourankim <yourankim.tech@gmail.com>
2020-02-01 20:21:35 +0900

    update: yummy button onclick event 상위 컴포넌트가 넘겨준 props의 핸들러 함수로 변경

--- src/components/ItemBox.js
+++ src/components/ItemBox.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function ItemBox(props) {
   const item = props.item;
-  const yummyOnClick = function () {
-    alert("냠냠. 1개 다 먹었다.");
+  
+  const onClickYummy = function() {
+   alert("잘 먹을게. 냠냠!");
+   props.handleItemCount(props.item.id);
   }
+
   return (
     <div className="itemBox">
       <div className="itemBox-name">{item.name}</div>
@@ -12,7 +15,7 @@ function ItemBox(props) {
       <div>{item.amount}</div>
       <div>{item.created} 구입</div>
       <div>{item.count}개 남음</div>
-      <button className="itemBox-button" onClick={yummyOnClick}>yummy!</button>
+      <button className="itemBox-button" onClick={onClickYummy}>yummy!</button>
     </div>
   );
 }
