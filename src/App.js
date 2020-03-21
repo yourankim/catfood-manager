4b283755dc44eec46093230070d064dd0a8ec302
yourankim <yourankim.tech@gmail.com>
2020-03-22 00:37:39 +0900

    update: localStorage에 아이템 저장 및 불러오기(임시)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ItemBox from './components/ItemBox';
-import NewItemBox from './components/NewItemBox';
-import Profile from './components/Profile';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ItemBox from "./components/ItemBox";
+import NewItemBox from "./components/NewItemBox";
+import Profile from "./components/Profile";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { 
-      items:[],
-      isAddItem: false, 
+    this.state = {
+      items: [],
+      isAddItem: false
     };
     this.handleItemCount = this.handleItemCount.bind(this);
     this.handleItemAdd = this.handleItemAdd.bind(this);
   }
 
   componentDidMount() {
-    this.setState({items: this.getAllItems()});
+    this.setState({ items: this.getAllItems() || [] });
   }
 
   handleItemCount(itemId) {
     let items = this.state.items;
-    
-    for(let i = 0; i < items.length; i++) {
-      if(items[i].id !== itemId) continue;
+
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id !== itemId) continue;
 
       let newCount = items[i].count;
       newCount = newCount - 1;
-      if(newCount < 0) return;
+      if (newCount < 0) return;
       items[i].count = newCount;
       break;
-    } 
-    
+    }
+
     this.setState({ items: items });
   }
 
@@ -41,63 +40,61 @@ class App extends Component {
     let items = this.state.items;
     const nextId = items.length + 2;
     item.id = nextId;
-    items.push(item);
-    this.setState( { items: items, isAddItem: false} );
+    items = [...items, item];
+    this.setItemToLocalStorage(items);
+    this.setState({ items: items, isAddItem: false });
+  }
+
+  setItemToLocalStorage(items) {
+    try {
+      localStorage.setItem("catfood-items", JSON.stringify(items));
+    } catch (e) {
+      alert("데이터 저장 실패. 잠시 후 다시 시도하세요.");
+    }
   }
 
   getAllItems() {
-    let items = [
-      { id: 1,
-        name: "카루 클래식",
-        flavor: "치킨과 크랩",
-        amount: "150",
-        count: 3,
-        created: "2020-01-14",
-      },
-      { id: 2,
-        name: "캣츠파인푸드 파우치",
-        flavor: "캥거루",
-        amount: "85",
-        count: 10,
-        created: "2020-01-14",
-      },
-      { id: 3,
-        name: "알모네이쳐",
-        flavor: "치킨과 호박",
-        amount: "80",
-        count: 5,
-        created: "2020-01-14",
-      }
-    ];
+    let items;
+    try {
+      const data = localStorage.getItem("catfood-items");
+      items = data ? JSON.parse(data) : [];
+    } catch (e) {
+      alert("데이터 불러오기 실패. 잠시 후 다시 시도하세요.");
+      items = [];
+    }
 
     return items;
   }
 
-  
-
   render() {
     return (
       <fragment>
-        <Header/>
-        <Profile/>
-        {this.state.items && this.state.items.map(item => (
-          <ItemBox 
-            key={item.id} 
-            item={item}
-            className="itemBox"
-            handleItemCount={this.handleItemCount}
-          />
-        ))}
-        <div className="addItem">
-          <button className="addItem-button" onClick={() => (this.setState({ isAddItem : !this.state.isAddItem }))}>식량 추가하기</button>
-          {this.state.isAddItem && <NewItemBox handleItemAdd={this.handleItemAdd} />}
+        <Header />
+        <Profile />
+        {this.state.items &&
+          this.state.items.map(item => (
+            <ItemBox
+              key={item.id}
+              item={item}
+              className='itemBox'
+              handleItemCount={this.handleItemCount}
+            />
+          ))}
+        <div className='addItem'>
+          <button
+            className='addItem-button'
+            onClick={() => this.setState({ isAddItem: !this.state.isAddItem })}
+          >
+            식량 추가하기
+          </button>
+          {this.state.isAddItem && (
+            <NewItemBox handleItemAdd={this.handleItemAdd} />
+          )}
         </div>
-        <Footer/>
+        <Footer />
       </fragment>
-      
-    )
+    );
   }
-  
 }
 
 export default App;
