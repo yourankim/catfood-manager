807a4e7984a38fabfbb09b1185e9d84550292cb5
yourankim <yourankim.tech@gmail.com>
2020-04-06 01:05:58 +0900

    update: created date  input type(text -> date)

--- src/components/NewItemBox.js
+++ src/components/NewItemBox.js
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function NewItemBox(props) {
+  const [name, setName] = useState("");
+  const [flavor, setFlavor] = useState("");
+  const [amount, setAmount] = useState("");
+  const [created, setCreated] = useState("");
+  const [count, setCount] = useState("");
 
-  const [name, setName] = useState('');
-  const [flavor, setFlavor] = useState('');
-  const [amount, setAmount] = useState('');
-  const [created, setCreated] = useState('');
-  const [count, setCount] = useState('');
-
-  const onAddItem = (e) => {
+  const onAddItem = e => {
     e.preventDefault(); // 이벤트를 취소할 수 있는 경우 전파를 막지 않고 이벤트를 취소
     e.stopPropagation(); // 현재 이벤트 이후의 전파를 차단
 
-    if(!name) {
+    if (!name) {
       alert("식품명을 입력하세요.");
       return;
     }
-    if(!flavor) {
+    if (!flavor) {
       alert("맛을 입력하세요.");
       return;
     }
-    if(!amount) {
+    if (!amount) {
       alert("용량을 입력하세요.");
       return;
     }
-    if(!created) {
+    if (!created) {
       alert("구입일을 입력하세요.");
       return;
     }
-    if(!count) {
+    if (!count) {
       alert("개수를 입력하세요.");
       return;
     }
@@ -38,25 +37,55 @@ function NewItemBox(props) {
       flavor: flavor,
       amount: amount,
       created: created,
-      count: count,
+      count: count
     });
 
-    setName('');
-    setFlavor('');
-    setAmount('');
-    setCreated('');
-    setCount('');
-  }
+    setName("");
+    setFlavor("");
+    setAmount("");
+    setCreated("");
+    setCount("");
+  };
 
   return (
-    <div className="itemBox">
+    <div className='itemBox'>
       <form onSubmit={onAddItem}>
-        <input type="text" id="name" value={name} onChange={(v) => setName(v.target.value)} placeholder="식품명"></input>
-        <input type="text" id="flavor" value={flavor} onChange={(v) => setFlavor(v.target.value)} placeholder="맛"></input>
-        <input type="number" id="amount" value={amount} onChange={(v) => setAmount(v.target.value)} placeholder="용량(g)"></input>
-        <input type="text" id="created" value={created} onChange={(v) => setCreated(v.target.value)} placeholder="구입일"></input>
-        <input type="number" id="count" value={count} onChange={(v) => setCount(v.target.value)} placeholder="개수"></input>
-        <button type="submit">저장하기</button>
+        <input
+          type='text'
+          id='name'
+          value={name}
+          onChange={v => setName(v.target.value)}
+          placeholder='식품명'
+        ></input>
+        <input
+          type='text'
+          id='flavor'
+          value={flavor}
+          onChange={v => setFlavor(v.target.value)}
+          placeholder='맛'
+        ></input>
+        <input
+          type='number'
+          id='amount'
+          value={amount}
+          onChange={v => setAmount(v.target.value)}
+          placeholder='용량(g)'
+        ></input>
+        <input
+          type='date'
+          id='created'
+          value={created}
+          onChange={v => setCreated(v.target.value)}
+          placeholder='구입일'
+        ></input>
+        <input
+          type='number'
+          id='count'
+          value={count}
+          onChange={v => setCount(v.target.value)}
+          placeholder='개수'
+        ></input>
+        <button type='submit'>저장하기</button>
       </form>
     </div>
   );
